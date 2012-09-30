08945ab3fb7800563c502161a78e0a472fadeee2
Shridharan Chandramouli <csd_666@live.in>
2012-09-30 13:20:24 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArcadeVolley::Application.config.secret_token = '78605fd8ab92f7c3798d51574c591b099ec72854c0150c7945a673f1d0d4bf6a82ad7250d24ae65d9549a95d60901a02c109304c3e60030e7781831bbba6b204'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArcadeVolley::Application.config.session_store :cookie_store, key: '_arcade_volley_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArcadeVolley::Application.config.session_store :active_record_store
